2ef2dcf6022a3f6ca248a0e47f47ca68635bdc65
muhammadidrees <idreesiqbal104@gmail.com>
2020-11-29 16:37:58 +0500

    firebase login added

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import fire from '../core/Firebase'
+
 import {
   Grid,
   Row,
@@ -11,6 +13,34 @@ import {
 import { Card } from "components/Card/Card.jsx";
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+}
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
+  }
+
+  submitLogin = () => {
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(res => {
+      if (res.user.uid){
+        localStorage.setItem('user', JSON.stringify(res.user));
+        window.location.href = '/admin/dashboard';
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div className="content">
@@ -24,20 +54,20 @@ class Login extends Component {
                     <div className="col-12 col-md-12 col-lg-12">
                       <div class="form-group">
                         <label>Email</label>
-                        <input type="email" class="form-control" placeholder="Email"/>
+                        <input type="email" name="email" class="form-control" value={this.state.email} onChange={(event) => this.handleChange(event)} placeholder="Email"/>
                       </div>
                     </div>
 
                     <div className="col-12 col-md-12 col-lg-12">
                       <div class="form-group">
                         <label>Password</label>
-                        <input type="password" class="form-control" placeholder="Password"/>
+                        <input type="password" name="password"  className="form-control" value={this.state.password} onChange={(event) => this.handleChange(event)} placeholder="Password"/>
                       </div>
                     </div>
 
                     <div className="col-12 col-md-12 col-lg-12">
                         <div className="text-center">
-                            <button type="button" class="btn btn-fill btn-primary">Login</button>
+                            <a onClick={() => this.submitLogin()} class="btn btn-fill btn-primary">Login</a>
                         </div>
                     </div>
 
