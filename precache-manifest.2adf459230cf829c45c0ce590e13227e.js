a139971feea88f9185be1f341aa29a4b9cc304ca
muhammadidrees <idreesiqbal104@gmail.com>
2020-11-29 16:51:12 +0500

    Updates

---
+++ precache-manifest.2adf459230cf829c45c0ce590e13227e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc2a68f25d3c502aa450c1ca21ae04a",
+    "url": "/cargo-service/index.html"
+  },
+  {
+    "revision": "86a029198c66e5b0fe3c",
+    "url": "/cargo-service/static/css/2.6388f885.chunk.css"
+  },
+  {
+    "revision": "75a45452a46ceb8588c5",
+    "url": "/cargo-service/static/css/main.3bdc77a6.chunk.css"
+  },
+  {
+    "revision": "86a029198c66e5b0fe3c",
+    "url": "/cargo-service/static/js/2.f93fb03f.chunk.js"
+  },
+  {
+    "revision": "75a45452a46ceb8588c5",
+    "url": "/cargo-service/static/js/main.c1d3f35f.chunk.js"
+  },
+  {
+    "revision": "d3a85469280bde576477",
+    "url": "/cargo-service/static/js/runtime~main.02886c7b.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/cargo-service/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/cargo-service/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/cargo-service/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/cargo-service/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/cargo-service/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/cargo-service/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/cargo-service/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/cargo-service/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/cargo-service/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/cargo-service/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/cargo-service/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/cargo-service/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/cargo-service/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/cargo-service/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/cargo-service/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
